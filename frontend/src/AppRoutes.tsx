a1594372d2f1540ec97d4569021101e305680fa0
Zain <raja.zainulabadin@vendomnia.com>
2025-04-07 10:51:14 +0400

    Created ProtectedRoute

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -12,24 +12,23 @@ import BoardAdmin from "./components/BoardAdmin";
 import SuperAdmin from "./components/SuperAdmin";
 import BoardDriver from "./components/BoardDriver/BoardDriver";
 import RouteEstimateComponent from "./components/RouteEstimateComponent";
-// import Dashboard from "../pages/Dashboard";
 // import NotFound from "../pages/NotFound";
+
+const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
+  const { user } = useAuth();
+  return user ? element : <Navigate to="/login" replace />;
+};
 
 const AppRoutes = () => {
   const { user } = useAuth();
 
   const getUserBoard = () => {
     if (user) {
-      if (isSuperAdmin(user)) {
-        return <Navigate to="/superadmin" replace />;
-      } else if (isAdmin(user)) {
-        return <Navigate to="/admin" replace />;
-      } else if (isDriver(user)) {
-        return <Navigate to="/driver" replace />;
-      } else {
-        return <Navigate to="/login" replace />;
-      }
+      if (isSuperAdmin(user)) return <Navigate to="/superadmin" replace />;
+      if (isAdmin(user)) return <Navigate to="/admin" replace />;
+      if (isDriver(user)) return <Navigate to="/driver" replace />;
     }
+    return <Navigate to="/login" replace />;
   };
 
   return (
@@ -38,19 +37,33 @@ const AppRoutes = () => {
         path="/"
         element={!user ? <Navigate to="/login" replace /> : getUserBoard()}
       />
-      <Route path="/login" element={!user ? <Login /> : getUserBoard()} />
+      <Route
+        path="/login"
+        element={!user ? <Login /> : <Navigate to="/" replace />}
+      />
       <Route
         path="/dashboard"
         element={user ? getUserBoard() : <Navigate to="/login" replace />}
       />
-      <Route path="/superadmin" element={<SuperAdmin />} />
-      <Route path="/driver" element={<BoardDriver />} />
-      <Route path="/admin" element={<BoardAdmin />} />
+      <Route
+        path="/superadmin"
+        element={<ProtectedRoute element={<SuperAdmin />} />}
+      />
+      <Route
+        path="/driver"
+        element={<ProtectedRoute element={<BoardDriver />} />}
+      />
+      <Route
+        path="/admin"
+        element={<ProtectedRoute element={<BoardAdmin />} />}
+      />
+      <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
+      <Route
+        path="/profile"
+        element={<ProtectedRoute element={<Profile />} />}
+      />
 
-      <Route path="/home" element={<Home />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/profile" element={<Profile />} />
-
       <Route path="/api/estimate" element={<RouteEstimateComponent />} />
 
       {/* <Route path="*" element={<NotFound />} /> */}
