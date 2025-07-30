d69b51072d1c837c99b4a987f8bfb4fc606a34d5
Zain <raja.zainulabadin@vendomnia.com>
2025-07-30 18:00:02 +0400

    created tour types in frontend

---
+++ frontend/src/types/tour.type.ts
@@ -0,0 +1,104 @@
+export type CreateTour = {
+  tourDate: string;
+  startTime: string;
+  orderIds: number[];
+  driverId: number;
+  warehouseId: number;
+  comments: string | null;
+  routeColor: string;
+};
+
+export type TourData = {
+  routePolyline: [number, number][];
+  stops: Stop[];
+};
+
+// export type Stop = {
+//   location: { lat: number; lng: number };
+//   time: { arrival: string; departure: string };
+//   activities: { jobId: string; type: string }[];
+// };
+
+// ******************
+
+export interface LocationPoint {
+  lat: number;
+  lng: number;
+}
+
+export interface TimeWindow {
+  start: string; // ISO timestamp
+  end: string;
+}
+
+export interface Activity {
+  time: TimeWindow;
+  type: string; // e.g., "departure", "delivery"
+  jobId: string;
+  location: LocationPoint;
+}
+
+export interface Stop {
+  load: number[];
+  time: {
+    arrival: string;
+    departure: string;
+  };
+  distance: number;
+  location: LocationPoint;
+  activities: Activity[];
+}
+
+export interface SectionSummary {
+  length: number; // in meters
+  duration: number; // in seconds
+  baseDuration: number; // in seconds
+}
+
+export interface Coordinate {
+  lat: number;
+  lng: number;
+  z: number;
+}
+
+export interface Section {
+  summary: SectionSummary;
+  coordinates: Coordinate[];
+}
+
+export interface Geometry {
+  vehicleId: string;
+  sections: Section[]; // would have 1 object normally
+  stops: Stop[];
+}
+
+export interface RouteLeg {
+  lat: number;
+  lon: number;
+  location_id: string;
+  arr_time: string; // ISO timestamp
+  arr_date_time: string; // ISO timestamp
+}
+
+export interface LogisticsRoute {
+  to: RouteLeg;
+  from: RouteLeg;
+  geometry: Geometry;
+  order_id: string;
+  distance_to: number;
+  driving_time_to: number;
+}
+
+export type Tour = {
+  vehicleId: string;
+  typeId: string;
+  stops: Stop[];
+  statistic: any;
+  shiftIndex: number;
+};
+
+export interface TourResponse {
+  tour: Tour;
+  routes: LogisticsRoute[];
+  unassigned: string;
+}
